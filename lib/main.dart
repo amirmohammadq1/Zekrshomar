79dab650d5230726ce923f7339a7dc8f4aca2413
amirmohammadq1 <amirmohammadmollaei3@gmail.com>
2026-08-25 18:35:17 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -276,7 +276,7 @@ class _HomeScreenState extends State<HomeScreen>
     try {
       final hasVibrator = await Vibration.hasVibrator();
       if (hasVibrator) {
-        Vibration.vibrate(duration: 35, amplitude: 220);
+        Vibration.vibrate(duration: 55, amplitude: 255);
       }
     } catch (_) {
       // روی دستگاه‌هایی که پلاگین لرزش را پشتیبانی نمی‌کنند، همان
